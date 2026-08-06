3227ae83fe6beae2c88167deeff10f249acc2e77
avbravo <avbravo@avbravo-ThinkCentre-M70t-Gen-3>
2026-08-06 09:06:40 -0500

    first commit

---
+++ FreyaTheme/src/main/java/com/jettra/theme/freyatheme/FreyaThemeTheme.java
@@ -0,0 +1,13 @@
+package com.jettra.theme.freyatheme;
+
+public class FreyaThemeTheme {
+    public static class Template {
+        public static final String CustomCSS = "<style>\n"
+            + "/* FreyaThemeTheme Custom CSS */\n"
+            + "</style>\n";
+
+        public static final String CustomJS = "<script>\n"
+            + "/* Scripts are loaded from theme.json */\n"
+            + "</script>";
+    }
+}
---
+++ MetisTheme/src/main/java/com/jettra/theme/metistheme/MetisThemeTheme.java
@@ -0,0 +1,13 @@
+package com.jettra.theme.metistheme;
+
+public class MetisThemeTheme {
+    public static class Template {
+        public static final String CustomCSS = "<style>\n"
+            + "/* MetisThemeTheme Custom CSS */\n"
+            + "</style>\n";
+
+        public static final String CustomJS = "<script>\n"
+            + "/* Scripts are loaded from theme.json */\n"
+            + "</script>";
+    }
+}
